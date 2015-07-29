a205af1aa39098a6d78511fded16771c23724580
elaine <elaine-me@elaines-MacBook-Pro.local>
2015-07-29 17:24:16 -0600

    first push

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var app = express();
+var mongoose = require('mongoose');
+
+//Middleware
+app.use(bodyParser());
+app.use(cors());
+app.use(express.static(__dirname + '/public')); 
+
+
+// Connections
+var port = 3000;
+
+
+
+
+
+
+
+var mongooseUri = 'mongodb://localhost/urbanity';
+mongoose.connect(mongooseUri);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+ console.log('Mongoose connected to your soul on:', mongooseUri);
+})
+
+app.listen(port, function () {
+  console.log('This is port:', port);
+})
