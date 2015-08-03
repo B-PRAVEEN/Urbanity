e4aaedc2d4352a0ba171c6bde0ec8ff6ecba3b7e
Katie Cowles <katie.cowles14@gmail.com>
2015-08-03 15:28:49 -0600

    updated user schema

--- api/models/User.js
+++ api/models/User.js
@@ -1,4 +1,10 @@
-name: {
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var Schema = mongoose.Schema;
+var q = require('q');
+
+var userSchema = new Schema({
+    name: {
         first: {type: String, trim: true, default: '', validate: [validateLocalStrategyProperty, 'Please fill in your first name']},
         last: {type: String, trim: true, default: '', validate: [validateLocalStrategyProperty, 'Please fill in your last name']}
     },
@@ -62,7 +68,7 @@ name: {
         default: '',
         validate: [validateLocalStrategyPassword, 'Password should be at least 6 characters']
     },
-    
+});
 
 
 
@@ -99,6 +105,39 @@ name: {
     resetPasswordExpires: {
         type: Date
     },*/
+
+
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var Schema = mongoose.Schema;
+var q = require('q');
+
+//pre('save') is mongoose middleware that runs before every user is created
+userSchema.pre('save', function(next) {
+    var user = this;
+    //take password and encrypt it
+    bcrypt.genSalt(10, function(err, salt) {
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            console.log(hash);
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.verifyPassword = function(password) {
+    var deferred = q.defer();
+    var user = this;
+    bcrypt.compare(password, user.password, function(err, res) {
+        if (err) {
+            deferred.resolve(false);
+        }
+        deferred.resolve(true);
+    });
+    return deferred.promise;
+};
+
+module.exports = mongoose.model('User', userSchema);
 
 
 
--- server.js
+++ server.js
@@ -5,104 +5,12 @@ var cors = require('cors');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var env = require('./env.js');
-var port = process.env.PORT || env.port;
+var port = 3000;
 
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-var FacebookStrategy = require('passport-facebook').Strategy;
-var TwitterStrategy = require('passport-twitter').Strategy;
 
 // used for endpoint security, checks for valid passport authenticated user
 // example:
 // app.get('/api/userProfile', isAuthed, function(){ ...(this will not fire if isAuthed fails) })
-var currentUser = {};
-
-var isAuthed = function (req, res, next) {
-  if (req.isAuthenticated()) {
-    //console.log(req);
-    currentUser = req.user;
-  /*  console.log('authenticated:')
-    console.log(currentUser.name)*/
-    return next();
-  }
-  console.log('Not authenticated');
-  res.status(401).end();
-}
-
-
-
-
-
-
-passport.serializeUser(function(user, done) {
-  //TODO serialize user id from database
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  //TODO query database with id
-  done(null, obj)
-})
-
-passport.use(new GoogleStrategy({
-      clientID: env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.googleClientSecret || env.googleClientSecret,
-      callbackURL: process.env.googleCallbackURL || env.googleCallbackURL,
-      passReqToCallback: true
-  },
-  function(req, token, tokenSecret, profile, done){
-    // Successful authentication, create or update user.
-    console.log('authenticated with google');
-    console.log(profile);
-    /*User.updateOrCreate(profile).then(function(results){
-      //console.log(req.session);
-      done(null, profile);
-  }, function(err){
-      done(err, profile);*/
-      done(null, profile);
-  })
-);
-
-passport.use(new TwitterStrategy({
-    consumerKey: process.env.CONSUMER_KEY || env.twitterConsumerKey,
-    consumerSecret: process.env.CONSUMER_SECRET || env.twitterConsumerSecret,
-    callbackURL: process.env.twitterCallbackURL || env.twitterCallbackURL
-  },
-  function(token, tokenSecret, profile, cb) {
-    console.log('auth with twitter');
-    console.log('profile');
-    // In this example, the user's Twitter profile is supplied as the user
-    // record.  In a production-quality application, the Twitter profile should
-    // be associated with a user record in the application's database, which
-    // allows for account linking and authentication with other identity
-    // providers.
-    return cb(null, profile);
-  }));
-
-passport.use(new FacebookStrategy({
-  clientID: env.FACEBOOK_APP_ID,
-    clientSecret: env.FACEBOOK_APP_SECRET,
-    callbackURL: env.facebookCallbackURL
-  },
-  function(accessToken, refreshToken, profile, done) {
-    // Successful authentication, create or update user.
-      //console.log(profile);
-      console.log('auth with facebook');
-      console.log(profile);
-/*    User.updateOrCreate(profile).then(function(results){
-
-        req.login(profile, function(err) {
-        if (err) {
-          return done(err, profile);
-        }
-        console.log('Should be logged in');
-      });*/
-      //console.log(req.session.passport.user);
-      done(null, profile);
-  }, function(err){
-      done(err, profile);
-  })
-  );
 
 
 // Connections
@@ -113,58 +21,6 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-
-//Session and Passport
-app.use(session({
-  secret: process.env.SESSION_SECRET || env.session_secret,
-  saveUninitialized: true,
-  resave: true
-}))
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-//TODO add authentication endpoints
-
-app.get('/auth/google', passport.authenticate('google', {scope: 'https://www.googleapis.com/auth/plus.login'}));
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }),
-  function(req, res) {
-
-    //successful authentication redirect, redirect user to welcome screen.
-    // User.getUser(req.user.id).then(function(userProfile) {
-    // currentUser = userProfile;
-    // })
-    // //console.log(currentUser);
-    // return res.redirect('/#/welcome');
-    // //res.status(200).json(req.user);
-    console.log('logged in with google')
-  });
-
-app.get('/auth/facebook', passport.authenticate('facebook'));
-app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/' }),
-  function(req, res) {
-    // //console.log(req.session);
-    // //console.log(req.isAuthenticated());
-    // //successful authentication redirect, redirect user to welcome screen.
-    // console.log('req.user: ');
-    // console.log(req.user);
-    // User.getUser(req.user.id).then(function(userProfile) {
-    // currentUser = userProfile;
-    // })
-    // //console.log(currentUser);
-    // return res.redirect('/#/welcome');
-    // //res.status(200).json(req.user);
-    console.log('logged in with facebook')
-  });
-
-app.get('/auth/twitter', passport.authenticate('twitter'));
-app.get('/auth/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-    console.log('logged in with twitter')
-    // res.redirect('/');
-  });
-
 
 // API endpoints
 // here
